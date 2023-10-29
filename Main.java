6f7d168a3b4b7219aee5e3d3150bd71d10d4fac2
Igor Guilherme Almeida Rocha <igorguilhermedev@gmail.com>
2023-10-29 19:22:25 -0300

    test: testing on main

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
+import java.util.Arrays;
+
+import enumerator.OrdenationType;
+import utils.Utils;
+
 public class Main{
     public static void main(String[] args) {
         int[] arr = {64, 25, 12, 22, 11, 9, 17, 0 ,6};
-        System.out.println("Array não ordenado:");
-        for (int value : arr) {
-            System.out.print(value + " ");
-        }
+        Arrays.stream(Utils.generateRandomIntArray(25, 0, 25, OrdenationType.RANDOM)).forEach((number) -> System.out.print(number + " "));
+        System.out.println("Crescente");
+        Arrays.stream(Utils.generateRandomIntArray(25, 0, 25, OrdenationType.ASCENDING)).forEach((number) -> System.out.print(number + " "));
+        System.out.println("Descrescente");
+        Arrays.stream(Utils.generateRandomIntArray(25, 0, 25, OrdenationType.DESCENDING)).forEach((number) -> System.out.print(number + " "));
 
-        InsertionSort.insertionSort(arr);
-
-        System.out.println("\nArray ordenado usando Selection Sort:");
-        for (int value : arr) {
-            System.out.print(value + " ");
-        }
     }
 }
